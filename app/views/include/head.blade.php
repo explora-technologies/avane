680d617f00179861cadb0ed47d08029110bd12bc
Rakesh.P.D <errakeshpd@gmail.com>
2014-11-29 14:41:24 +0530

    Include view folders

---
+++ app/views/include/head.blade.php
@@ -0,0 +1,10 @@
+    <title>Avane Global Pvt Ltd</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Rakesh.P.D">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <!-- CSS Global Compulsory -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
