0bd01064761af9ab393c71ad878a7a68c78416a5
Rakesh.P.D <errakeshpd@gmail.com>
2014-11-30 04:43:00 +0530

    about page included and icons added

---
+++ app/views/pages/about.blade.php
@@ -0,0 +1,21 @@
+ @extends('layouts.main')
+
+ @section('content')
+<div class="col-md-12">
+<div class="container">		
+    <div class="col-md-6 margin">
+        <h2 class="heading-md ">ABOUT OUR COMPANY</h2>
+        <p class="text-layout-justify">For enterprise level content management system, we use Liferay or Adobe CQ5 and for collaboration portals our choice is Sharepoint. We use Microsoft .NET and Sun Java J2EE framework to build complex business applications for our large clients. For small to medium businesses, our preference is Symfony PHP framework for web solutions. Our open source team uses Typo3 CMS when it comes to CMS driven projects and Magento commerce for internet shopping solutions. .</p>
+    </div>
+    <div class="col-md-6 left-border margin">
+        <h2 class="heading-md ">Our mission</h2>
+        <p class="text-layout-justify">World class solutions for clients business.</p>
+        <h2 class="heading-md ">Our vision</h2>
+        <p class="text-layout-justify">World class solutions for clients business.</p>
+        <h2 class="heading-md ">Quality Policy</h2>
+        <p class="">World class solutions for clients business.</p>
+    </div>
+</div>
+</div>
+
+@stop
