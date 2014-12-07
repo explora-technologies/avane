a14a4476f7150c719d72c1ac5643952a031d1679
Rakesh.P.D <errakeshpd@gmail.com>
2014-12-07 08:35:29 +0530

    mobility content modified

--- app/views/pages/practices/mobility.blade.php
+++ app/views/pages/practices/mobility.blade.php
@@ -15,5 +15,22 @@
     </div><!--/breadcrumbs-->
     <!--=== End Breadcrumbs ===-->
 </div>
-
+<div class="col-md-12">
+    <div class="main-header">
+        Go Mobile
+    </div>
+<div class="subtitle">
+    Engage your customers directly and handle IoT and big data with ease.
+</div>
+<div class="main_content">Mobile technology is revolutionizing the way businesses interact with end-users,
+ whether the end-user is a customer, employee, business partner, or a smart device like IP enabled Air conditioner, 
+ Television or an Automobile. This flood of data traffic from mobile phones and other IP enabled fragmented devices 
+ is ever growing exponentially and organizations face a proliferation of data to store and analyze. The collection 
+ and use of such large data flows has the potential to completely transform your business models and services. 
+To tap into this opportunity many organizations are leveraging data analytics in operations that range from optimizing 
+the value chain and manufacturing production to improving customer relationships. To innovate and to remain 
+competitive in this rapidly growing mobile society, we help develop comprehensive mobile  applications platform
+ for your business. We are here to help you smoothly embrace this rapidly evolving mobile landscape. 
+</div>
+</div>
 @stop
