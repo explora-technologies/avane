4fec23a2dbbed68d1d52ee1335259e21a6a3f3a9
Rakesh.P.D <errakeshpd@gmail.com>
2014-12-07 08:45:31 +0530

    gaming services content modified

--- app/views/pages/service/GamingServices.blade.php
+++ app/views/pages/service/GamingServices.blade.php
@@ -14,5 +14,26 @@
     </div><!--/breadcrumbs-->
     <!--=== End Breadcrumbs ===-->
 </div>
+<div class="col-md-12">
+    <div class="main-header">
+      Gaming Platform
+    </div>
+<div class="subtitle">
+   Where the winner settle
+</div>
+<div class="main_content">We offer highly-entertaining, top-performance, end-to-end gaming solutions to
+ operators in the PC/Console gaming, e-learning, web and mobile platforms. Our service ranges from 
+ conceptualization, design consulting in the early stages of game development, to testing and integration.
+  We deliver top-quality gaming artifacts including graphic design, character development, animation,
+   level design, programming, physics, game engine and testing.
 
+Our core team has strong expertise in Flash, 3ds MAX, Action Script 3.0, Flash Lite, J2ME, JSR 184, FBML, 
+Gamebryo and Unity3D technologies and have extensive experience in developing flash, 3d games for Mobile, PC,
+ Web and Console.
+
+We thrive by delivering always on time, meeting the needs of our clients and end-gamers using all possible resources, 
+tools, technologies and skills. It is our mission to create fun and action packed next-Gen games and to entice the 
+world of gamers young and old alike. We are into developing 3D game apps for Android and IPhone.
+ </div>
+</div>
 @stop
