fab04b26c370674bc5ef65c17f277fc0ddac84b2
Rakesh.P.D <errakeshpd@gmail.com>
2014-12-13 16:24:54 +0530

    navigation design modified

--- app/views/pages/aboutus/team.blade.php
+++ app/views/pages/aboutus/team.blade.php
@@ -19,17 +19,10 @@
 <div class="col-md-12">
 <div class="container">		
     <div class="col-md-6 margin">
-        <h2 class="heading-md ">AVANE MANAGEMENT TEAM</h2>
-        <p class="text-layout-justify">For enterprise level content management system, we use Liferay or Adobe CQ5 and for collaboration portals our choice is Sharepoint. We use Microsoft .NET and Sun Java J2EE framework to build complex business applications for our large clients. For small to medium businesses, our preference is Symfony PHP framework for web solutions. Our open source team uses Typo3 CMS when it comes to CMS driven projects and Magento commerce for internet shopping solutions. .</p>
+        <h2 class="heading-md ">Share the content</h2>
+        <p class="text-layout-justify"></p>
     </div>
-    <div class="col-md-6 left-border margin">
-        <h2 class="heading-md ">Our mission</h2>
-        <p class="text-layout-justify">To realize our dreams, our vision by efficient delivery of services, honesty and integrity in all aspect of our business, respect towards our team, clients and other business entities we deal or partner with.</p>
-        <h2 class="heading-md ">Our vision</h2>
-        <p class="text-layout-justify">To be a well respected and leading IT services company delivering cost effective and high quality technology services, projects and products.</p>
-        <h2 class="heading-md ">Quality Policy</h2>
-        <p class="">Avane Technologies is committed to serve and satisfy our customers by surpassing their business needs by delivering excellent products, services and intuitive outstanding design. We strive to meet or exceed our customer expectations through our strong commitment for continual improvement of our quality management system, process and services, in compliance with the statutory and legal requirements.</p>
-    </div>
+   
 </div>
 </div>
 <div class="col-md-12" style="height:50px"></div>
