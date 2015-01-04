a39e38318c2671071323309b856525b85fd18c3a
Rakesh.P.D <errakeshpd@gmail.com>
2015-01-04 09:28:27 +0530

    footer modified

--- app/views/include/footer.blade.php
+++ app/views/include/footer.blade.php
@@ -1,3 +1,43 @@
-<div class="col-md-12 border-black-1px text-center footer-padding footer-color">
+<div class="col-md-12" style="background-color:#585F69;padding:1%; color:white">
+    <div class="container">
+           <div class="col-md-4">
+                    <!-- About -->
+                    <div class="headline"><h4>About</h4></div>  
+                    <p class="margin-bottom-25 md-margin-bottom-40" style="font-size:12px">For enterprise level content management system, we use Liferay or Adobe CQ5 and for collaboration portals our choice is Sharepoint. We use Microsoft .NET and Sun Java J2EE framework to build complex business applications for our large clients. For small to medium businesses, our preference is Symfony PHP framework for web solutions. Our open source team uses Typo3 CMS when it comes to CMS driven projects and Magento commerce for internet shopping solutions.</p>    
+                    <!-- End About -->
+                </div><!--/col-md-4-->  
+
+                <div class="col-md-4">
+                    <!-- Recent Blogs -->
+                   
+                    <!-- Social Links -->
+                    <div class="headline"><h4>Stay Connected</h4></div> 
+                    <ul style="none" style="font-size:12px">
+                        <li><a href="http://facebook.com/" data-original-title="Feed" class="social_rss">Facebook</a></li>
+                        <li><a href="http://plus.google.com" data-original-title="Facebook" class="social_facebook">Google +</a></li>
+                        <li><a href="#" data-original-title="Twitter" class="social_twitter">Twitter</a></li>
+                    </ul>
+                    <!-- End Social Links -->
+                    <!-- End Recent Blogs -->                    
+                </div><!--/col-md-4-->
+
+                <div class="col-md-4">
+                    <!-- Contact Us -->
+                    <div class="headline"><h4>Contact Us</h4></div> 
+                    <address class="md-margin-bottom-40" style="font-size:12px">
+                       Avane Tech Pvt LTD <br>
+                       B9, Kanniah street  <br>
+                       Near Ambika Empire<br>
+                       Vadapalani, chennai <br>
+                        Email: <a href="mailto:avane@gmail.com" class="">avane@gmail.com</a>
+                <br>Mobile: 9746929688 .09111111111111<br>8111111111111
+                    </address>
+                    <!-- End Contact Us -->
+
+                </div><!--/col-md-4--> 
+    </div>
+</div>
+<div class="col-md-12 border-black-1px text-center footer-padding footer-color" style="background: none repeat scroll 0% 0% #3E4753;
+border-top: 1px solid #777;">
 Copyright @ Avane Global Pvt Ltd
 </div>
